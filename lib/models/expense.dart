ad3863835fa01e7f4b9b212b2fe7432013b6f518
fares2718 <faresobaid2715@gmail.com>
2026-06-15 13:30:28 +0300

    Refactor expenses management: create Expenses widget, add ExpensesList and ExpenseItem components, and update dependencies

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category {food,travel,leisure,work}
+
+const categoryIcon = {
+  Category.food: Icons.lunch_dining,
+  Category.leisure: Icons.movie,
+  Category.travel: Icons.flight_takeoff,
+  Category.work: Icons.work
+};
 
 class Expense {
   Expense({required this.title, required this.amount, required this.date,required this.category})
@@ -12,4 +23,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
